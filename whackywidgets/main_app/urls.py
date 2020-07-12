c7853fa3603accdfb19236c516c81a5ab654b297
R <50966008+rashmika13@users.noreply.github.com>
2020-07-12 14:03:13 -0400

    CSS updated

--- whackywidgets/main_app/urls.py
+++ whackywidgets/main_app/urls.py
@@ -2,17 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # widget index
     path('', views.widget_index, name='widget_index'),
 
     # widget form
     path('widgets/add_widget', views.add_widget, name='add_widget'),
-    # create widget
-    path('widgets/create/', views.WidgetCreate.as_view(), name='widget_create'),
 
+    # delete widget
     path('widgets/<int:widget_id>/delete/',
          views.widget_delete, name='widget_delete'),
-
-    # path('widgets/<int:pk>/delete/',
-    #      views.WidgetDelete.as_view(), name='widget_delete'),
 
 ]
--- whackywidgets/main_app/views.py
+++ whackywidgets/main_app/views.py
@@ -4,7 +4,6 @@ from django.views.generic import ListView
 from .forms import WidgetForm
 from django.views.generic.edit import CreateView, DeleteView
 from django.db.models import Sum
-# Create your views here.
 
 
 def widget_index(request):
@@ -14,14 +13,6 @@ def widget_index(request):
     widget_form = WidgetForm()
     return render(request, 'widgets/index.html', {'widgets': widgets, 'widget_form': widget_form, 'total_quantity': total_quantity})
 
-# class WidgetList(ListView):
-#     model = Widget
-
-
-class WidgetCreate(CreateView):
-    model = Widget
-    fields = '__all__'
-
 
 def widget_delete(request, widget_id):
     w = Widget.objects.get(id=widget_id)
@@ -30,7 +21,6 @@ def widget_delete(request, widget_id):
 
 
 def add_widget(request):
-    print('Its working')
     form = WidgetForm(request.POST)
     if form.is_valid():
         new_widget = form.save(commit=False)
